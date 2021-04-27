7ba136f6a181a9185ffebd5ecfd6898e491e9d30
brunofsabino <brunoferrazsabino@gmail.com>
2021-04-27 12:00:49 -0300

    Update Config.php

--- Cadastro_MVC/src/Config.php
+++ Cadastro_MVC/src/Config.php
@@ -2,7 +2,7 @@
 namespace src;
 
 class Config {
-    const BASE_DIR = '/BFS Desenvolvimento/Cadastro_MVC/public';
+    const BASE_DIR = '/Cadastro_MVC/public';
 
     const DB_DRIVER = 'mysql';
     const DB_HOST = 'localhost';
