7ce471a512925229a204a3d4b86336e20694aaa0
brunofsabino <brunoferrazsabino@gmail.com>
2021-05-28 10:52:45 -0300

    petshop

---
+++ PetShopFeliz/index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pet Shop Feliz</title>
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Sigmar+One&display=swap" rel="stylesheet">
+</head>
+<body>
+    <header>
+        <div class="header container">
+            <div class="logo">
+                <img src="assets/images/logo.png" alt="logo pet shop feliz">
+            </div>
+            <nav>
+                <ul>
+                    <li><a href="">Cachorros</a></li>
+                    <li><a href="">Gatos</a></li>
+                    <li><a href="">Banho e Tosa</a></li>
+                    <li><a href="">Outros</a></li>
+                    <li><a href="">Fale Conosco</a></li>
+                </ul>
+            </nav>
+        </div>
+    </header>
+    <main>
+        <section class="banner">
+            <div class="container slidearea">
+                <div class="slideshow">
+                aaa
+                </div>
+            </div>
+        </section>
+    </main>
+    <footer>
+
+    </footer>
+</body>
+</html>
