934a6df64c266a7ffdc4c7c6031d34972dcffc1c
brunofsabino <brunoferrazsabino@gmail.com>
2021-06-05 12:03:25 -0300

    coaching

--- CoachingSeguros/index.php
+++ CoachingSeguros/index.php
@@ -8,6 +8,33 @@
     <link rel="stylesheet" href="assets/css/style.css">
 </head>
 <body>
-    
+    <header>
+        <div class="header container">
+            <div class="logo">
+                <img src="assets/images/logo-redi.jpeg" alt="logo">
+            </div>
+            <div class="menu-section on ">
+                <div class="menu-toggle">
+                    <div class="one"></div>
+                    <div class="two"></div>
+                    <div class="three"></div>
+                </div>
+                <nav>
+                    <ul>
+                        <li><a href="">Home</a></li>
+                        <li><a href="">Sobre</a></li>
+                        <li><a href="">Planos de Saúde</a></li>
+                        <li><a href="">Seguros</a></li>
+                        <li><a href="">Contato</a></li>
+                    </ul>
+        
+                </nav>
+            </div>
+        </div>
+    </header>
+    <main>
+    </main>
+    <footer>
+    </footer>
 </body>
 </html>
