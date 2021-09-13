add1460699ee564d0fcf3750bd81a5cb9f59b87d
brunofsabino <brunoferrazsabino@gmail.com>
2021-09-13 11:25:09 -0300

    GNDI

---
+++ CoachingSeguros/GNDI/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Notre Dame Intermédica</title>
+</head>
+<body>
+    <img src="assets/images/bg-01.svg" alt="" class="bg-01">
+</body>
+</html>
