772fa2130973b2feaf537afc916e5114d24ab755
brunofsabino <brunoferrazsabino@gmail.com>
2021-09-13 17:57:52 -0300

    GNDI

--- CoachingSeguros/GNDI/index.php
+++ CoachingSeguros/GNDI/index.php
@@ -5,8 +5,92 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Notre Dame Intermédica</title>
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700;900&display=swap" rel="stylesheet">
 </head>
 <body>
     <img src="assets/images/bg-01.svg" alt="" class="bg-01">
+    <header>
+        <div class="header-menu">
+            <div class="header-menu-logo">
+                <img src="assets/images/LOGO_NOTREDAME.png" alt="">
+            </div>
+            <div class="header-menu-nav">
+                <nav>
+                    <ul>
+                        <li><a href="/">Home</a></li>
+                        <li><a href="/">Sobre</a></li>
+                        <li><a href="/">Calcular Plano</a></li>
+                        <li><a href="/">Contato</a></li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+    </header>
+    <section class="section-01">
+        <div class="section-01-bg">
+            <div class="section-01-bg-escuro">
+                <div class="section-01-content">
+                    <div class="section-01-content-text">
+                        <div class="section-01-text">
+                            <h1>Preencha os campos e receba os valores dos planos da NotreDame Intermédica</h1><br>
+                            <h1>Central de Relacionamento <br>(11) 2539-6687</h1>
+                            <div class="area-button--whats">
+                                <div class="button--whats">
+                                    <img src="assets/images/icons8-whatsapp-48.png" alt="" style="width:30px;height: 30px">
+                                    FALAR COM A GNDI
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="section-01-content-formulario">
+                        <div class="section-01-formulario">
+                            <form action="">
+                                <p>Digite seus dados:</p><br><br>
+                                <label for="">Nome:<br>
+                                    <input type="text" class="input-text" placeholder="Digite seu nome">
+                                </label><br>
+                                <label for="">DDD + Telefone:<br>
+                                    <input type="text" class="input-text" placeholder="Digite seu telefone">
+                                </label><br>
+                                <label for="">E-mail:<br>
+                                    <input type="email" class="input-text" placeholder="Digite seu e-mail">
+                                </label><br>
+                                <label for="">Você tem CNPJ?:<br>
+                                    <select name="possui-cnpj" id="">
+                                        <option value="0">Selecione</option>
+                                        <option value="1">Sim</option>
+                                        <option value="2">Não</option>
+                                    </select>
+                                </label><br>
+                                <label for="">Número de Vidas:<br>
+                                    <select name="numero-vidas" id="">
+                                        <option value="0">Selecione</option>
+                                        <option value="1">1</option>
+                                        <option value="2">2</option>
+                                        <option value="3">3</option>
+                                        <option value="4">4</option>
+                                        <option value="5">5</option>
+                                        <option value="6">6</option>
+                                        <option value="7">7</option>
+                                        <option value="8">8</option>
+                                        <option value="9">9</option>
+                                        <option value="Mais de 9">+ de 9</option>
+                                    </select>
+                                </label><br>
+                                <label for="">Idades:<br>
+                                    <input type="email" class="input-text" placeholder="Digite as Idades">
+                                </label><br>
+                                <button>Enviar</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    
 </body>
 </html>
